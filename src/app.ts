a979992e0db4438aa037380faa4d89a0e725c663
luan menezesds <luanmenezesds615@gmail.com>
2026-03-10 22:10:30 -0300

    Integrating express

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,12 @@
-console.log("Rodando");
+import express from "express";
+import config from "config";
+
+const app = express();
+
+app.use(express.json());
+
+const port = config.get<number>("port");
+
+app.listen(3000, async () =>{
+  console.log("Aplicação está funcionando na porta: ${port}");
+});
